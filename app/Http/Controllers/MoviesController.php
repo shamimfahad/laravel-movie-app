c6b3f8cccd871128621e8c51b6e6a6f966960ab1
shamimfahad <shamimfahadshuvo@gmail.com>
2020-06-04 00:14:38 +0600

    added alpine js for bringing up modals and toggling between searched outcomes in the dropdown, also added a few key bindings to input and body

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -5,105 +5,113 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
-class MoviesController extends Controller {
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function index() {
-		$popularMovies = Http::withToken(config('services.tmdb.token'))
-			->get('https://api.themoviedb.org/3/movie/popular')
-			->json()['results'];
+class MoviesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $popularMovies = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/movie/popular')
+            ->json()['results'];
 
-		$nowPlayingMovies = Http::withToken(config('services.tmdb.token'))
-			->get('https://api.themoviedb.org/3/movie/now_playing')
-			->json()['results'];
+        $nowPlayingMovies = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/movie/now_playing')
+            ->json()['results'];
 
-		$genresArray = Http::withToken(config('services.tmdb.token'))
-			->get('https://api.themoviedb.org/3/genre/movie/list')
-			->json()['genres'];
+        $genresArray = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/genre/movie/list')
+            ->json()['genres'];
 
-		$genres = collect($genresArray)->mapWithKeys(function ($genre) {
-			return [$genre['id'] => $genre['name']];
-		});
-		
-		return view(
-			'index',
-			[
-				'popularMovies' => $popularMovies,
-				'nowPlayingMovies' => $nowPlayingMovies,
-				'genres' => $genres,
-			]
-		);
-	}
+        $genres = collect($genresArray)->mapWithKeys(function ($genre) {
+            return [$genre['id'] => $genre['name']];
+        });
 
-	
+        return view(
+            'index',
+            [
+                'popularMovies' => $popularMovies,
+                'nowPlayingMovies' => $nowPlayingMovies,
+                'genres' => $genres,
+            ]
+        );
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function create() {
-		//
-	}
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store(Request $request) {
-		//
-	}
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function show($id) {
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
 
-		$movie = Http::withToken(config('services.tmdb.token'))
-			->get('https://api.themoviedb.org/3/movie/'.$id.'?append_to_response=credits,videos,images')
-			->json();
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
 
-		return view('show', [
-			'movie' => $movie 
-		]);
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function edit($id) {
-		//
-	}
+        $movie = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/movie/' . $id . '?append_to_response=credits,videos,images')
+            ->json();
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function update(Request $request, $id) {
-		//
-	}
+        return view('show', [
+            'movie' => $movie
+        ]);
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function destroy($id) {
-		//
-	}
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,16 +1,27 @@
-<div class="relative mt-3 md:mt-0">
-    <input wire:model.debounce.500ms="search" type="text" class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
+<div class="relative mt-3 md:mt-0" x-data="{ isOpen: true }" @click.away="isOpen = false">
+    <input wire:model.debounce.500ms="search" type="text"
+        class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
+        placeholder="Search" @keydown.window="
+            if (event.keyCode === 191) {
+                event.preventDefault();
+                $refs.search.focus();
+            }" x-ref="search" @focus="isOpen = true" @keydown.escape.window="isOpen = false"
+        @keydown.shift.tab="isOpen = false" @keydown="isOpen = true">
     <div class="absolute top-0 py-1 px-2">
         <i class="fas fa-search fill-current text-gray-500"></i>
     </div>
     <div wire:loading class="spinner top-0 right-0 mr-4 mt-3"></div>
     @if (strlen($search) >= 2)
-    <div class="absolute bg-gray-800 text-sm rounded w-64 mt-4">
+    <div class="z-50 absolute bg-gray-800 text-sm rounded w-64 mt-4" x-show.transition.opacity="isOpen">
         @if ($searchResults->count() > 0)
         <ul>
             @foreach ($searchResults as $result)
             <li class="border-b border-gray-700">
-                <a href="{{ route('movies.show', $result['id']) }}" class="block hover:bg-gray-700 px-3 py-3 flex items-center">
+                <a href="{{ route('movies.show', $result['id']) }}"
+                    class="block hover:bg-gray-700 px-3 py-3 flex items-center" @if ($loop->last)
+                    @keydown.tab="isOpen = false"
+                    @endif
+                    >
                     @if ($result['poster_path'])
                     <img src="{{'https://image.tmdb.org/t/p/w92/'.$result['poster_path'] }}" alt="poster" class="w-8">
                     @else
